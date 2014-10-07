00d55039472689d4367a431aa926d616ae848677
bojanisc <bojan.isc@infigo.hr>
2014-10-07 05:06:49 +0200

    first commit

---
+++ splunk-rsync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if we are the active node
+#  DEVICE must be up and have an IP address assigned
+DEVICE="eth0:0"
+
+# Splunk home
+SPLUNK_HOME="/opt/splunk"
+
+# Secondary node
+SECONDARY_NODE="192.168.1.1"
+
+
+lines="`/sbin/ifconfig $DEVICE | grep inet | wc -l`"
+
+if [ $lines == "0" ]
+then
+        # We are not the active node
+        # Exit gracefully
+        exit
+fi
+
+# Else, we are the active node
+# Rsync SPLUNK_HOME to the other node
+/bin/rsync -az $SPLUNK_HOME/ splunk@$SECONDARY_NODE:$SPLUNK_HOME/
